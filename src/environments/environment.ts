d7273a811d432874877a032fb0ae075c327de7a7
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com>
2024-09-13 02:15:24 +0100

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://13.61.4.9/api/v1/',
+  apiUrl: '13.61.4.9/api/v1/',
   auth0Domain: 'dev-efimao22k2abduzg.us.auth0.com',
   clientId: 'D4Hs1Rs6IOmgHijYXAoWkkTkMvXweBtH',
   audience: 'https://dev-efimao22k2abduzg.us.auth0.com/api/v2/',
