6b2f860319eb2dc7b6ff239998c5db1dfe2a779c
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com>
2024-09-13 02:52:15 +0100

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '//ec2-13-61-4-9.eu-north-1.compute.amazonaws.com/api/v1/',
+  apiUrl: 'https://j1dm17x6g5.execute-api.eu-north-1.amazonaws.com/somuga/api/v1/',
   auth0Domain: 'dev-efimao22k2abduzg.us.auth0.com',
   clientId: 'D4Hs1Rs6IOmgHijYXAoWkkTkMvXweBtH',
   audience: 'https://dev-efimao22k2abduzg.us.auth0.com/api/v2/',
