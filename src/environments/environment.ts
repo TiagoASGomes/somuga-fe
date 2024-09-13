31ba250aaf3e6ef878f4a9fac642da0ac535c9f2
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com>
2024-09-13 02:12:30 +0100

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-13-60-37-140.eu-north-1.compute.amazonaws.com:8080/api/v1/',
+  apiUrl: 'http://13.61.4.9/api/v1/',
   auth0Domain: 'dev-efimao22k2abduzg.us.auth0.com',
   clientId: 'D4Hs1Rs6IOmgHijYXAoWkkTkMvXweBtH',
   audience: 'https://dev-efimao22k2abduzg.us.auth0.com/api/v2/',
